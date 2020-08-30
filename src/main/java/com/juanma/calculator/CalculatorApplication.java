ec47a107538bebc6d68dc802fc1cf25973f85afe
Juanma <jmlpdr.93@gmail.com>
2020-08-30 21:57:05 +0100

    Multibranch Jenkins

--- src/main/java/com/juanma/calculator/CalculatorApplication.java
+++ src/main/java/com/juanma/calculator/CalculatorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
-	// Un cambio
+	// Cambio en nueva rama
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
