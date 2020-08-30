6b2fd4f986afe0a3767e81243cc5fbe08576d67e
Juanma <jmlpdr.93@gmail.com>
2020-08-30 17:46:30 +0100

    Otro cambio

--- src/main/java/com/juanma/calculator/CalculatorApplication.java
+++ src/main/java/com/juanma/calculator/CalculatorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
-
+	// Un cambio
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
